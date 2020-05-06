e847ab510a0ad83c23accb7eca753f8405e49324
William Lopes <williamlopes64@gmail.com>
2020-05-06 13:50:07 -0300

    Adicionando a classe NextEvolution

---
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/model/Pokemon.java
@@ -0,0 +1,25 @@
+package williamlopes.requisicoeshttp.pokedex.model;
+
+import java.util.List;
+
+class Pokemon {
+
+    public int id;
+    public String num;
+    public String name;
+    public String img;
+    public List<String> type;
+    public String height;
+    public String weight;
+    public String candy;
+    public int candy_count;
+    public String egg;
+    public double spawn_chance;
+    public double avg_spawns;
+    public String spawn_time;
+    public List<double> multipliers;
+    public List<String> weaknesses;
+    public List<NextEvolution> next_evolution;
+    public List<PrevEvolution> prev_evolution;
+
+}
