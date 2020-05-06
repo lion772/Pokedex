33f94dd02dc19b572a9ef81fb048f72bbc349cb5
William Lopes <williamlopes64@gmail.com>
2020-05-06 13:19:56 -0300

    Adicionando a classe Pokedex

---
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/model/Pokedex.java
@@ -0,0 +1,21 @@
+package williamlopes.requisicoeshttp.pokedex.model;
+
+public class Pokedex
+{
+    private List<Pokemon> pokemon;
+
+    public Pokedex() {
+    }
+
+    public Pokedex(List<Pokemon> pokemon) {
+        this.pokemon = pokemon;
+    }
+
+    public List<Pokemon> getPokemon() {
+        return pokemon;
+    }
+
+    public void setPokemon(List<Pokemon> pokemon) {
+        this.pokemon = pokemon;
+    }
+}
