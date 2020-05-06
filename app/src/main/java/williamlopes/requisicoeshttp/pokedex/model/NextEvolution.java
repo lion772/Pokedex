8c3e9f97c0db99f046e78e06b0a8d3739f32fbcd
William Lopes <williamlopes64@gmail.com>
2020-05-06 13:54:47 -0300

    adicionando os atributos da classe NextEvolution

--- app/src/main/java/williamlopes/requisicoeshttp/pokedex/model/NextEvolution.java
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/model/NextEvolution.java
@@ -1,4 +1,22 @@
 package williamlopes.requisicoeshttp.pokedex.model;
 
 class NextEvolution {
+    public String num;
+    public String name;
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
