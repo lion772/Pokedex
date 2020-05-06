1a2c9307b27a1ba9694d33942dc2d23254f4533b
William Lopes <williamlopes64@gmail.com>
2020-05-06 13:53:39 -0300

    adicionando os atributos da classe PrevEvolution

--- app/src/main/java/williamlopes/requisicoeshttp/pokedex/model/PrevEvolution.java
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/model/PrevEvolution.java
@@ -1,4 +1,22 @@
 package williamlopes.requisicoeshttp.pokedex.model;
 
 class PrevEvolution {
+    public String num;
+    public String name;
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
