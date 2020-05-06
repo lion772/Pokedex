ac1f2c17b12f5d43b0ce63f1ac43c174cb62b7dc
William Lopes <williamlopes64@gmail.com>
2020-05-06 14:50:05 -0300

    Adicionando Blank Fragment para receber a lista de pokémons

---
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/PokemonList.java
@@ -0,0 +1,29 @@
+package williamlopes.requisicoeshttp.pokedex;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PokemonList extends Fragment {
+
+    public PokemonList() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+}
