9e1923312a8832abbc87bfcc53cdd8cbf1ccc0b6
William Lopes <williamlopes64@gmail.com>
2020-05-06 23:53:58 -0300

    Adicionando List<Pokemon> na classe Common.java

--- app/src/main/java/williamlopes/requisicoeshttp/pokedex/Common/Common.java
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/Common/Common.java
@@ -1,4 +1,10 @@
 package williamlopes.requisicoeshttp.pokedex.Common;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import williamlopes.requisicoeshttp.pokedex.model.Pokemon;
+
 public class Common {
+    public static List<Pokemon> commonPokemonList = new ArrayList<>();
 }
