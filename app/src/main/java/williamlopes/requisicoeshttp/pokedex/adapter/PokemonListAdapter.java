33bd418f8b2601e04722d7b232331c09d2e8bef6
William Lopes <williamlopes64@gmail.com>
2020-05-06 22:11:45 -0300

    adicionando PokemonListAdapter

--- app/src/main/java/williamlopes/requisicoeshttp/pokedex/adapter/PokemonListAdapter.java
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/adapter/PokemonListAdapter.java
@@ -1,4 +1,61 @@
 package williamlopes.requisicoeshttp.pokedex.adapter;
 
-public class PokemonListAdapter {
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import williamlopes.requisicoeshttp.pokedex.R;
+import williamlopes.requisicoeshttp.pokedex.model.Pokemon;
+
+public class PokemonListAdapter extends RecyclerView.Adapter<PokemonListAdapter.MyViewHolder> {
+
+    Context context;
+    List<Pokemon> listaPokemons;
+
+    public PokemonListAdapter(Context context, List<Pokemon> listaPokemons) {
+        this.context = context;
+        this.listaPokemons = listaPokemons;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.pokemon_list_item, parent, false);
+        return new MyViewHolder( itemLista );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        Glide.with(context).load(listaPokemons.get(position).getImg()).into(holder.pokemon_image);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaPokemons.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView pokemon_image;
+        TextView pokemon_name;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            pokemon_image = itemView.findViewById(R.id.pokemon_image);
+            pokemon_name = itemView.findViewById(R.id.text_pokemon_name);
+        }
+    }
 }
