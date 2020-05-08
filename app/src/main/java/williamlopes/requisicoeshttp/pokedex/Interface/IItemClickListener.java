8c4a81fe37d4d5ea284e8692f3cd77f4b5ea5263
William Lopes <williamlopes64@gmail.com>
2020-05-07 22:32:26 -0300

    Adicionando a interface IItemClickListener para adicionar evento de click na recyclerview

---
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/Interface/IItemClickListener.java
@@ -0,0 +1,8 @@
+package williamlopes.requisicoeshttp.pokedex.Interface;
+
+import android.view.View;
+
+public interface IItemClickListener extends View.OnClickListener {
+    void onClick(View view, int position);
+
+}
