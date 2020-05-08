c1a223df9bb709fe1a5dbc6b28200b1df43e6464
William Lopes <williamlopes64@gmail.com>
2020-05-08 15:29:03 -0300

    Adicionando PokemonTypeAdapter.java

---
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/adapter/PokemonTypeAdapter.java
@@ -0,0 +1,76 @@
+package williamlopes.requisicoeshttp.pokedex.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.robertlevonyan.views.chip.Chip;
+import com.robertlevonyan.views.chip.OnChipClickListener;
+
+import java.util.List;
+
+import williamlopes.requisicoeshttp.pokedex.Interface.IItemClickListener;
+import williamlopes.requisicoeshttp.pokedex.R;
+
+public class PokemonTypeAdapter extends RecyclerView.Adapter<PokemonTypeAdapter.MyViewHolder> {
+
+
+    Context context;
+    List<String> listaTypes;
+
+    public PokemonTypeAdapter(Context context, List<String> listaTypes) {
+        this.context = context;
+        this.listaTypes = listaTypes;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(context).inflate(R.layout.chip_item, parent, false);
+        return new MyViewHolder( itemLista );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.chip.setChipText(listaTypes.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaTypes.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+        Chip chip;
+        IItemClickListener iItemClickListener;
+
+        public void setiItemClickListener(IItemClickListener iItemClickListener) {
+            this.iItemClickListener = iItemClickListener;
+        }
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            chip = itemView.findViewById(R.id.chip);
+            chip.setOnChipClickListener(new OnChipClickListener() {
+                @Override
+                public void onChipClick(View v) {
+                    iItemClickListener.onClick(v, getAdapterPosition());
+                }
+            });
+
+
+        }
+
+
+
+}
+
+}
