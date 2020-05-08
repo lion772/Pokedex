d4fe0c140b1dce57752a6fc5db22f130b99ce925
William Lopes <williamlopes64@gmail.com>
2020-05-07 22:55:41 -0300

    Adicionando o fragment PokemonDetail.java e sua .xml

---
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/PokemonDetail.java
@@ -0,0 +1,29 @@
+package williamlopes.requisicoeshttp.pokedex;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PokemonDetail extends Fragment {
+
+    public PokemonDetail() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_pokemon_detail, container, false);
+        return view;
+    }
+}
