426fa2d02d21b2ac2047b3072e590f54947c07fe
William Lopes <williamlopes64@gmail.com>
2020-05-07 23:40:53 -0300

    Recebendo a broadcast do adapter, habilitando o botão de voltar na Toolbar, substituindo a fragment e setando o nome do pokemon na Toolbar

--- app/src/main/java/williamlopes/requisicoeshttp/pokedex/MainActivity.java
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/MainActivity.java
@@ -2,17 +2,53 @@ package williamlopes.requisicoeshttp.pokedex;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import williamlopes.requisicoeshttp.pokedex.Common.Common;
+import williamlopes.requisicoeshttp.pokedex.model.Pokemon;
+
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    BroadcastReceiver showDetail = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().toString().equals(Common.KEY_ENABLE_HOME)){
+                //Habilita o botão de voltar na Toolbar em ambos os casos
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+                //Substituir o fragment
+                Fragment detailFragment = PokemonDetail.getInstance();
+                int position = intent.getIntExtra("position", -1);
+                Bundle bundle = new Bundle();
+                bundle.putInt("position", position);
+                detailFragment.setArguments(bundle);
+
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.list_pokemon_fragment, detailFragment);
+                fragmentTransaction.addToBackStack("detail");
+                fragmentTransaction.commit();
+
+                //Setar nome do pokémon na Toolbar
+                Pokemon pokemon = Common.commonPokemonList.get(position);
+                toolbar.setTitle(pokemon.getName());
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("POKEMON LISTA");
         setSupportActionBar(toolbar);
     }
