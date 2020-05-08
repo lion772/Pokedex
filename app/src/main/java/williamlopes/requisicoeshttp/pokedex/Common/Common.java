7a226c9dafb3326594dc5d7a9cc09db905b90106
William Lopes <williamlopes64@gmail.com>
2020-05-07 23:38:17 -0300

    Adicionando String KEY_ENABLE_HOME

--- app/src/main/java/williamlopes/requisicoeshttp/pokedex/Common/Common.java
+++ app/src/main/java/williamlopes/requisicoeshttp/pokedex/Common/Common.java
@@ -1,5 +1,7 @@
 package williamlopes.requisicoeshttp.pokedex.Common;
 
+import android.content.Intent;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,4 +9,5 @@ import williamlopes.requisicoeshttp.pokedex.model.Pokemon;
 
 public class Common {
     public static List<Pokemon> commonPokemonList = new ArrayList<>();
+    public static final String KEY_ENABLE_HOME = "enable_home";
 }
